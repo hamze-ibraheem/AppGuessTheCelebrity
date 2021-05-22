11f920bd48203410c1db90e35ecaba041238a52a
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-22 06:35:56 +0300

    Pick up randoum celeb Image and display it into image view.

--- app/src/main/java/com/taskfoundation/appguessthecelebrity/MainActivity.java
+++ app/src/main/java/com/taskfoundation/appguessthecelebrity/MainActivity.java
@@ -2,22 +2,38 @@ package com.taskfoundation.appguessthecelebrity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+
+    int choosenCeleb = 0;
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
 
         DownloadTask task = new DownloadTask();
         String result = null;
@@ -25,6 +41,33 @@ public class MainActivity extends AppCompatActivity {
             result = task.execute("https://www.imdb.com/list/ls052283250/").get();
 
             Log.i("Content is: ", result);
+
+            String[] splitResult = result.split("<div class=\"listedArticles\"></div>");
+
+            //Regex
+            Pattern pattern = Pattern.compile("img src=\"(.*?)\"");
+            Matcher matcher = pattern.matcher(splitResult[0]);
+
+            while (matcher.find()) {
+                celebURLs.add(matcher.group(1));
+            }
+
+            pattern = Pattern.compile("alt=\"(.*?)\"");
+            matcher = pattern.matcher(splitResult[0]);
+
+            while (matcher.find()) {
+                celebNames.add(matcher.group(1));
+            }
+
+            Random rand = new Random();
+            choosenCeleb = rand.nextInt(celebURLs.size());
+
+            ImageDownloader imageDownloaderTask = new ImageDownloader();
+
+            Bitmap celebImage = imageDownloaderTask.execute(celebURLs.get(choosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -56,6 +99,27 @@ public class MainActivity extends AppCompatActivity {
                     data = reader.read();
                 }
                 return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                return bitmap;
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
